a5fd243c388f912d7e44228bf4596195595ef918
Fabian Schuiki <fabian@schuiki.ch>
2026-03-19 12:36:44 -0700

    [sv-tests] Limit make parallelism
    Change `make -j` into `make -j $(nprocs)` to limit parallelism in CI,
    which may be causing some non-deterministic failures or out-of-memory
    kills.

--- verilog/sv-tests/run.sh
+++ verilog/sv-tests/run.sh
@@ -3,7 +3,7 @@ set -e
 cd "$(dirname "${BASH_SOURCE[0]}")/../../ext/sv-tests"
 export RUNNERS_FILTER=circt_verilog
 make clean
-make -j generate-tests
-make -j tests
+make -j $(nproc) generate-tests
+make -j $(nproc) tests
 make report
 ../../verilog/sv-tests/stats.sh
